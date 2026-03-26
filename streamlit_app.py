545002e07965f0c79d5137a185dd68757181a7c1
madhumita070507 <165452707+madhumita070507@users.noreply.github.com>
2026-03-26 16:55:49 +0530

    Refactor smoothie ingredient API call handling
    Removed redundant API call and dataframe display for watermelon.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get(
-               "https://my.smoothiefroot.com/api/fruit/watermelon"+ fruit_chosen
+               "https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen
 )
 
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
@@ -38,8 +38,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
 
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
