7d989157f8a0529483ad468d034e8401f3c91081
madhumita070507 <165452707+madhumita070507@users.noreply.github.com>
2026-03-28 16:23:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,43 +42,30 @@ max_selections=5
 # If ingredients are selected
 
 if ingredients_list:
+   ingredients_string = ''
 
-```
-ingredients_string = ''
+   for fruit_chosen in ingredients_list:
 
-for fruit_chosen in ingredients_list:
-
-    ingredients_string += fruit_chosen + ' '
+      ingredients_string += fruit_chosen + ' '
 
     # Get API search value from SEARCH_ON column
-    search_on = pd_df.loc[
-        pd_df['FRUIT_NAME'] == fruit_chosen,
-        'SEARCH_ON'
-    ].iloc[0]
+      search_on = pd_df.loc[ pd_df['FRUIT_NAME'] == fruit_chosen,'SEARCH_ON'].iloc[0]
 
-    st.subheader(fruit_chosen + ' Nutrition Information')
+      st.subheader(fruit_chosen + ' Nutrition Information')
 
     # API call
-    smoothiefroot_response = requests.get(
-        "https://my.smoothiefroot.com/api/fruit/" + search_on
-    )
+      smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
 
-    st.dataframe(
-        data=smoothiefroot_response.json(),
-        use_container_width=True
-    )
+      sf_df=st.dataframe( data=smoothiefroot_response.json(),use_container_width=True)
 
-st.write(ingredients_string)
+#st.write(ingredients_string)
 
 # Insert order into Snowflake
-my_insert_stmt = f"""
-    INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-    VALUES ('{ingredients_string.strip()}', '{name_on_order}')
-"""
+  my_insert_stmt = f"""INSERT INTO smoothies.public.orders(ingredients, name_on_order)VALUES ('{ingredients_string.strip()}', '{name_on_order}')"""
 
-time_to_insert = st.button('Submit Order')
+  time_to_insert = st.button('Submit Order')
 
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
-```
+  if time_to_insert:
+      session.sql(my_insert_stmt).collect()
+      st.success('Your Smoothie is ordered!', icon="✅")
+
